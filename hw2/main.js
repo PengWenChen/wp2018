5640889f07591d20b93ba7c1bc9266e60232f1b3
PengWenChen <domokun0413@gmail.com>
2018-10-03 14:27:25 +0800

    js change

--- hw2/main.js
+++ hw2/main.js
@@ -20,7 +20,7 @@ var $slides_2 = $('.slide_2', $slider_2);
 var $slider_bg = $('#slider_bg');
 var $slideContainer_bg = $('.slides_bg', $slider_bg);
 var $slides_bg = $('.slide_bg', $slider_bg);
-
+var $w=$('.word3').text;
 
 $('.arrow').click( function(){
     $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
@@ -49,6 +49,19 @@ $('.arrow').click( function(){
             $slideContainer_bg.css('margin-left', 0);
         }
     });
+    var c=currentSlide_bg+2;
+    if(currentSlide_bg==3){
+        c=1;
+    }else if(currentSlide_bg==4){
+        c=2;
+    }
+    var $w=$('.pic1.word'+c)
+    if(c!=1){
+        $('#arise').text($w.text());        
+    }else{
+        $('#arise').text('desk&chair');  
+    }
+
 
 
 });
